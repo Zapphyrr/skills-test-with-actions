d66947391fe7af39a96e840424aa7aa4c93c3b5b
Waphos <114501626+Zapphyrr@users.noreply.github.com>
2025-11-13 16:17:51 +0000

    ajout de deux fonctions traitant le cas d'un nombre négatif

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,3 +68,26 @@ def test_get_nth_fibonacci_ten():
 
     # Assert
     assert result == 55
+
+
+#Ajout par moi-même d'une fonction qui traite le cas d'un rayon négatif
+def test_area_of_circle_negative_radius():
+   """Test with a negative radius to raise ValueError."""
+   # Arrange
+   radius = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      area_of_circle(radius)
+
+
+#Ajout par moi-même d'une fonction qui traite le cas d'un nombre fibonacci
+#négatif
+def test_get_nth_fibonacci_negative():
+   """Test with a negative number to raise ValueError."""
+   # Arrange
+   n = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      get_nth_fibonacci(n)
